1504738cfaf33487699a4bc5f38eff2d41413b5f
Nick <nickleeh@hotmail.com>
2015-01-04 18:39:31 +0800

    Set default coding system as utf-8.

--- init.el
+++ init.el
@@ -243,6 +243,19 @@
 
 (setq py-load-pymacs-p  'nil)
 
+
+;; Here's a pretty comprehensive group of magic invocations to make Emacs use UTF-8 everywhere by default:
+  (setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+  (set-language-environment 'utf-8)
+  (set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+  (setq locale-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (unless (eq system-type 'windows-nt)
+   (set-selection-coding-system 'utf-8))
+(prefer-coding-system 'utf-8)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide '.emacs)
 ;;; .emacs ends here
